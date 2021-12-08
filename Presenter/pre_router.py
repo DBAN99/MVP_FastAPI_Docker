b6327ff4e6cedbaa300cf5e5ad3ec8b3350f6767
DBAN <dban7171@gmail.com>
2021-12-08 20:24:06 +0900

    Chore : router 이름 변경
    pre_router로 변경

--- Presenter/pre_router.py
+++ Presenter/pre_router.py
@@ -1,9 +1,9 @@
 from View import api_get,api_post,api_patch,api_delete
 from fastapi import APIRouter
 
-api_router = APIRouter
+router = APIRouter
 
-api_router.include_router(api_get.router, tags=["READ"])
-api_router.include_router(api_post.router, tags=["CREATE"])
-api_router.include_router(api_patch.router, tags=["UPDATE"])
-api_router.include_router(api_delete.router, tags=["DELETE"])
+router.include_router(api_get.router, tags=["READ"])
+router.include_router(api_post.router, tags=["CREATE"])
+router.include_router(api_patch.router, tags=["UPDATE"])
+router.include_router(api_delete.router, tags=["DELETE"])
